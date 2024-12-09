a40cc32e9e810c56b3108f2a801e697fbe8d0f6d
Daniel Swanson <awesomeevildudes@gmail.com>
2024-12-09 17:39:43 -0500

    more rule types; start on context position explanations
    adapt multi_option from -rtx

--- code2text_cg/grammar.py
+++ code2text_cg/grammar.py
@@ -1,10 +1,33 @@
 from code2text.translate import Pattern
 from tree_sitter_apertium import CG
 
+def multi_option(*args, lists=None, cap=True):
+    from itertools import product
+    ops = []
+    for cond, out in args:
+        if cond:
+            ops.append([
+                ([{'has': cond}], out),
+                ([], ''),
+            ])
+        else:
+            ops.append([([], out)])
+    ret = []
+    for seq in product(*ops):
+        cond = []
+        out = ''
+        for c, o in seq:
+            cond += c
+            out += o
+        if cap and out:
+            out = out[0].upper() + out[1:]
+        ret.append({'cond': cond, 'output': out, 'lists': (lists or {})})
+    return ret
+
 base_rules = [
     {'pattern': '[(semicolon) (comment)] @root', 'output': ''},
     {
-        'pattern': '(source_file (_) @thing_list) @root',
+        'pattern': '(source_file (_)* @thing_list) @root',
         'output': [
             {
                 'lists': {
@@ -34,27 +57,16 @@ base_rules = [
   (rule
     (ruletype) @type
     (rule_target (_) @target)
-    [(contexttest) @test_list "(" ")"]*
+    [(contexttest)* @test_list "(" ")"]*
   ) @root
   (#eq? @type "SELECT")
 )
 ''',
-        'output': [
-            {
-                'cond': [
-                    {'has': 'test_list'}
-                ],
-                'output': 'If {test_list}, keep only readings matching {target}.',
-                'lists': {
-                    'test_list': {
-                        'join': ' and '
-                    }
-                }
-            },
-            {
-                'output': 'Keep only readings matching {target}'
-            }
-        ]
+        'output': multi_option(
+            ('test_list', 'if {test_list}, '),
+            (None, 'keep only readings matching {target}.'),
+            lists={'test_list': {'join': ' and '}},
+        ),
     },
     {
         'pattern': '''
@@ -62,31 +74,136 @@ base_rules = [
   (rule
     (ruletype) @type
     (rule_target (_) @target)
-    [(contexttest) @test_list "(" ")"]*
+    [(contexttest)* @test_list "(" ")"]*
   ) @root
   (#eq? @type "REMOVE")
 )
 ''',
-        'output': [
-            {
-                'cond': [
-                    {'has': 'test_list'}
-                ],
-                'output': 'If {test_list}, remove any readings matching {target}.',
-                'lists': {
-                    'test_list': {
-                        'join': ' and '
-                    }
-                }
+        'output': multi_option(
+            ('test_list', 'if {test_list}, '),
+            (None, 'remove any readings matching {target}.'),
+            lists={'test_list': {'join': ' and '}},
+        ),
+    },
+    {
+        'pattern': '''
+(
+  (rule
+    (ruletype) @type
+    (rule_target (_) @target)
+    [(contexttest)* @test_list "(" ")"]*
+  ) @root
+  (#eq? @type "UNMAP")
+)
+''',
+        'output': multi_option(
+            ('test_list', 'if {test_list}, '),
+            (None, 'remove tags and restrictions added by MAP for words matching {target}.'),
+            lists={'test_list': {'join': ' and '}},
+        ),
+    },
+    {
+        'pattern': '''
+        (
+          (rule
+            (ruletype) @type
+            (rule_target (_) @target)
+            [(contexttest)* @test_list "(" ")"]*
+          ) @root
+          (#eq? @type "REMCOHORT")
+        )
+        ''',
+        'output': multi_option(
+            ('test_list', 'if {test_list}, '),
+            (None, 'delete words matching {target}.'),
+            lists={'test_list': {'join': ' and '}},
+        ),
+    },
+    {
+        'pattern': '''
+        (
+          (rule_substitute_etc
+            (ruletype_substitute_etc) @type
+            (inlineset) @src
+            (inlineset) @trg
+            (rule_target (_) @target)
+            [(contexttest)* @test_list "(" ")"]*
+          ) @root
+          (#eq? @type "SUBSTITUTE")
+        )
+        ''',
+        'output': multi_option(
+            ('test_list', 'if {test_list}, '),
+            (None, 'replace {src} with {trg} in readings matching {target}.'),
+            lists={'test_list': {'join': ' and '}},
+        ),
+    },
+    {
+        'pattern': '''
+        (
+          (rule_parentchild
+            type: (ruletype_parentchild) @type
+            trg: (rule_target (_) @target)
+            context: (contexttest)* @test_list
+            (contexttest) @ctxtarget
+            (contexttest)* @ctx_list
+          ) @root
+          (#eq? @type "SETPARENT")
+        )
+        ''',
+        'output': multi_option(
+            ('test_list', 'if {test_list}, '),
+            (None, 'set the parent of words matching {target} to {ctxtarget}.'),
+            lists={'test_list': {'join': ' and '}},
+        ),
+    },
+    {
+        'pattern': '''
+        (
+          (rule_map_etc
+            type: (ruletype_map_etc) @type
+            tags: (inlineset) @tags
+            target: (rule_target (_) @target)
+            context: (contexttest)* @test_list
+          ) @root
+          (#eq? @type "MAP")
+        )
+        ''',
+        'output': multi_option(
+            ('test_list', 'if {test_list}, '),
+            (None, 'add {tags} to {target} and prevent other rules from adding tags.'),
+            lists={'test_list': {'join': ' and '}},
+        ),
+    },
+    {
+        'pattern': '''
+        (
+          (rule_with
+            trg: (rule_target (_) @target)
+            context: (contexttest)* @test_list
+            children: (_)* @rule_list
+          ) @root
+        )
+        ''',
+        'output': multi_option(
+            (None, 'find a word matching {target}'),
+            ('test_list', ' in context {test_list}'),
+            (None, ' and run the following rules:\n  {rule_list}'),
+            lists={
+                'test_list': {'join': ' and '},
+                'rule_list': {'join': '\n  '},
             },
-            {
-                'output': 'Keep only readings matching {target}'
-            }
-        ]
+        ),
     },
     {
         'pattern': '(section_header) @root',
         'output': 'Start a new section.',
+    },
+    {
+        'pattern': '''(inlineset
+          (inlineset_single . (taglist (tag) @t (#eq? @t "*")) .)
+        ) @root''',
+        'output': 'any word',
     },
     {
         'pattern': '''
@@ -126,7 +243,7 @@ base_rules = [
         ],
     },
     {
-        'pattern': '(taglist (tag) @tag_list) @root',
+        'pattern': '(taglist (tag)* @tag_list) @root',
         'output': [{
             'lists': {'tag_list': {'join': ', '}},
             'output': 'tags {tag_list}',
@@ -137,26 +254,42 @@ base_rules = [
         'output': '{tags}',
     },
     {
-        'pattern': '(compotag "(" (tag) @tag_list ")") @root',
+        'pattern': '(compotag "(" (tag)* @tag_list ")") @root',
         'output': [{
             'lists': {'tag_list': {'join': ' and '}},
             'output': '({tag_list})',
         }],
     },
     {
-        'pattern': '(list (setname) @name_text (taglist [(tag) (compotag)] @tag_list)) @root',
+        'pattern': '(list (setname) @name_text (taglist [(tag) (compotag)]* @tag_list)) @root',
         'output': [{
             'lists': {'tag_list': {'join': ' or '}},
             'output': 'Define the set {name_text} as matching {tag_list}.',
         }],
     },
     {
-        'pattern': '(contexttest (contextpos) @ctx_text (inlineset (_) @set) (LINK) (contexttest) @link) @root',
-        'output': 'the word at position {ctx_text} matches {set} and, relative to that, {link}',
+        'pattern': '(contexttest (contextpos) @ctx (inlineset (_) @set) (LINK) (contexttest) @link) @root',
+        'output': '{ctx} matches {set} and, relative to that, {link}',
     },
     {
-        'pattern': '(contexttest (contextpos) @ctx_text (inlineset (_) @set) !link) @root',
-        'output': 'the word at position {ctx_text} matches {set}',
+        'pattern': '(contexttest (contextpos) @ctx (inlineset (_) @set) !link) @root',
+        'output': '{ctx} matches {set}',
+    },
+    {
+        'pattern': '(contextpos . (ctx_parent) .) @root',
+        'output': 'the parent',
+    },
+    {
+        'pattern': '(contextpos . (ctx_sibling) .) @root',
+        'output': 'a sibling',
+    },
+    {
+        'pattern': '(contextpos . (ctx_child) .) @root',
+        'output': 'a child',
+    },
+    {
+        'pattern': '(contextpos) @root_text',
+        'output': 'the word at position {root_text}',
     },
     {
         'pattern': '''
@@ -169,6 +302,10 @@ base_rules = [
     {
         'pattern': '(inlineset (inlineset_single) @a (set_op) @op_text (inlineset_single) @b) @root',
         'output': '{a} {op_text} {b}',
+    },
+    {
+        'pattern': '(setname) @root_text',
+        'output': '{root_text}',
     },
 ]
 
